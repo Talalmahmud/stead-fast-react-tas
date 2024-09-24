5860fab2e94cf55bcd411d31354fe056020aea11
Talal Mahmud <mahmudtalal2@gmail.com>
2024-09-24 22:24:34 +0600

    mobile responsive added

--- src/component/MobileNavBar.jsx
+++ src/component/MobileNavBar.jsx
@@ -1,7 +1,54 @@
 import React from "react";
 
-const MobileNavBar = () => {
-  return <div>MobileNavBar</div>;
+const MobileNavBar = ({ setClose }) => {
+  return (
+    <div className=" fixed pb-[20px] bg-slate-500 top-0 right-0 md:hidden flex flex-col  z-20 w-[300px] min-h-screen  gap-4  text-white text-[16px]">
+      <div
+        onClick={() => setClose(false)}
+        className=" absolute top-4 rounded-full text-black right-4 w-[24px] h-[24px] bg-white flex items-center justify-center text-[14px]"
+      >
+        x
+      </div>
+      <div className=" pt-[60px]">
+        <a
+          className=" border-b-[2px] border-slate-500 px-2 hover:border-white"
+          href="/"
+        >
+          Home
+        </a>
+      </div>
+      <div>
+        <a
+          className=" border-b-[2px] border-slate-500 px-2 hover:border-white"
+          href="/"
+        >
+          Services
+        </a>
+      </div>
+      <div>
+        <a
+          className=" border-b-[2px] border-slate-500 px-2 hover:border-white"
+          href="/"
+        >
+          Contact Us
+        </a>
+      </div>
+      <div>
+        <a
+          className=" border-b-[2px] border-slate-500 px-2 hover:border-white"
+          href="/"
+        >
+          About Us
+        </a>
+      </div>
+      <div>
+        {" "}
+        <button className=" bg-white px-2 py-[4px] text-black rounded-[4px]">
+          Login
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default MobileNavBar;
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import MobileNavBar from "./MobileNavBar";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <div className=" px-4 w-full flex items-center justify-between py-4 bg-black">
       <div>
@@ -9,7 +11,7 @@ const Navbar = () => {
         </a>
       </div>
 
-      <div className=" flex items-center gap-4 text-white text-[16px]">
+      <div className=" hidden md:flex items-center gap-4 text-white text-[16px]">
         <div>
           <a
             className=" border-b-[2px] border-black px-2 hover:border-white"
@@ -43,7 +45,18 @@ const Navbar = () => {
           </a>
         </div>
       </div>
-      <button className=" bg-white px-2 py-[4px]  rounded-[4px]">Login</button>
+      <button className=" hidden md:block bg-white px-2 py-[4px]  rounded-[4px]">
+        Login
+      </button>
+      <div
+        onClick={() => setToggle(true)}
+        className=" md:hidden flex flex-col gap-[4px] min-w-[24px]"
+      >
+        <div className=" w-full h-[2px] bg-white"></div>
+        <div className=" w-full h-[2px] bg-white"></div>{" "}
+        <div className=" w-full h-[2px] bg-white"></div>
+      </div>
+      {toggle && <MobileNavBar setClose={setToggle} />}
     </div>
   );
 };
