6235118e31d6b1386b0944bf14b2fef305a2ca64
Talal Mahmud <mahmudtalal2@gmail.com>
2024-09-25 00:14:52 +0600

    hover effect add on product card

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ProductShow from "./component/ProductShow";
 const App = () => {
   return (
     <div className=" w-full">
-      <div className=" w-[420px] md:w-[720px] xl:w-[1100px] mx-auto">
+      <div className=" w-[336px] sm:w-[420px] md:w-[720px] xl:w-[1100px] mx-auto">
         <Navbar />
         <HeroSection />
         <ProductShow />
--- src/component/ProductCard.jsx
+++ src/component/ProductCard.jsx
@@ -2,14 +2,16 @@ import React from "react";
 
 const ProductCard = ({ itemDetail }) => {
   return (
-    <div className=" bg-slate-300 rounded-[8px] p-2 ">
+    <div className=" hover:bg-gray-400 cursor-pointer hover:-translate-y-1 bg-slate-300 rounded-[8px] p-2 ">
       <img
         src="https://img.freepik.com/free-photo/wet-sphere-reflective-water-abstract-beauty-generated-by-ai_188544-19616.jpg"
         className=" h-[150px] w-full"
         alt=""
       />
       <p className=" text-[20px] font-semibold">{itemDetail?.title}</p>
-      <p>{itemDetail?.price}</p>
+      <p className=" text-[14px]">
+        Price: {itemDetail?.price} <span className=" text-green-700">BDT</span>
+      </p>
       <p className=" text-[14px]">{itemDetail?.description?.slice(0, 40)}...</p>
     </div>
   );
